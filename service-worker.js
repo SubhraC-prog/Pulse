111daef3b041bc085288f2ea9ec91bd48d3d68d6
SubhraC-prog <subhrachakraborty049@gmail.com>
2025-12-07 02:29:00 +0530

    sw js

---
+++ service-worker.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "pulse-cache-v1";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./Login.html",
+  "./Dashboard.html",
+  "./manifest.json",
+  "./Icon.png",
+  "./service-worker.js"
+];
+
+// Install SW
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      console.log("[ServiceWorker] Pre-caching assets");
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+// Activate SW
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(
+        keyList.map(key => {
+          if (key !== CACHE_NAME) {
+            console.log("[ServiceWorker] Removing old cache", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Fetch handler
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // Serve from cache when available
+      return response || fetch(event.request);
+    })
+  );
+});
